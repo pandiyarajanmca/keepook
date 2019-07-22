c83e1c7f292ad8c3cceb5d14f36b9496b707827d
Muthu <muthu.r@blockgemini.com>
2019-07-22 15:16:59 +0530

    issues

--- src/app/technical-settings/add-data-source/add-data-source.component.ts
+++ src/app/technical-settings/add-data-source/add-data-source.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+
 
 @Component({
   selector: 'app-add-data-source',
@@ -11,5 +12,5 @@ export class AddDataSourceComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
 }
