09e89fc6bd89badf1dade5f920e1aa75511db0b1
pandiyarajan_mca <pandiyarajan.tcps@gmail.com>
2019-08-06 16:52:24 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		this.authService.logout;
+		this.authService.logout();
 		this.loginForm = this.formBuilder.group({
 			usernameOrEmail: ["", Validators.required],
 			password: ["", Validators.required]
