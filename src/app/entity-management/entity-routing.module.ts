78a93ec204a089e09ff37e3912835111fd88aef6
Surya <nagusaladi20@gmail.com>
2019-08-08 11:38:54 +0530

    changes

--- src/app/entity-management/entity-routing.module.ts
+++ src/app/entity-management/entity-routing.module.ts
@@ -6,14 +6,14 @@ import { DepartmentComponent } from './department/department.component';
 import { OrganizationComponent } from './organization/organization.component';
 import { EntityManagementComponent } from './entity-management.component';
 import { ViewCompanyComponent } from './company/view-company/view-company.component';
-import { ViewOrganizationComponent } from './organization/view-organization/view-organization.component';
 import { ViewDepartmentComponent } from './department/view-department/view-department.component';
 import { ViewUserGroupComponent } from './user-group/view-user-group/view-user-group.component';
+import { ViewOrganizationComponent } from './organization/view-organization/view-organization.component';
 const routes: Routes = [
   { path: '', component: EntityManagementComponent },
   { path: 'add-company', component: CompanyComponent },
   { path: 'edit-view-company', component: ViewCompanyComponent },
-  { path: 'edit-view-organization', component: ViewOrganizationComponent },
+  { path: 'edit-view-organization', component: OrganizationComponent },
   { path: 'edit-view-department', component: ViewDepartmentComponent },
   { path: 'edit-view-user-groups', component: ViewUserGroupComponent },
   { path: 'add-organization', component: OrganizationComponent },
